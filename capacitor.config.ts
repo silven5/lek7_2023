d623dfc98c8452f737f66f812c94e66e0ef90b53
silven <elvenff@gmail.com>
2023-03-24 09:34:08 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'lek7.2023',
+  appName: 'lek7-2023',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
